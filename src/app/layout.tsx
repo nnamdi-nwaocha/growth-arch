00e7d151778c8621dad530cd64ac013065e7f76e
nnamdi nwaocha <nnamdinwaocha@gmail.com>
2024-12-09 11:02:22 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="./icons/favicon.png" type="image/x-icon" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-[#000]`}
       >
